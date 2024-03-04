c5e50bbf1762ef24eb038c0d2a99462bcdd75322
Li <li@silica.codes>
2024-03-04 19:03:51 +1300

    Update ida project, add texture2d test

--- IDA Project/Windows/MonoParse.py
+++ IDA Project/Windows/MonoParse.py
@@ -1,6 +1,7 @@
 import idc
+import ida_bytes
 import idaapi
-
+import ida_offset
 import struct
 
 def rename_function(ea, name):
@@ -26,30 +27,58 @@ def toCppName(csName):
     cppName = csName.replace(".", "::")
     return cppName.split("(")[0]
 
-def rename_tbl(tbl_start, tbl_end):
+def rename_tbl(tbl_start):
     
     # run over the thing
     offset = tbl_start
 
-    while offset < tbl_end:
-        dis = idc.GetDisasm(offset)
-        if dis.startswith("mov"):
-            pts = dis[len("mov     "):].split(',')
-            #print(pts)
-            addr = idc.get_name_ea_simple(pts[0])
-            newname = "_"+pts[1][len(" offset "):]
-            print(hex(addr))
-            print(newname)
-            rename_function(addr, newname)
-        offset += 0xA
+    while True:
+        nameptr = idc.get_wide_dword(offset)
+        funptr = idc.get_wide_dword(offset+4)
 
+        if nameptr == 0:
+            break
+        if funptr == 0:
+            break
+        
+        name = read_cstring(nameptr)
+
+#        thumbReg = funptr & 1
+#        funptr -= thumbReg
+#        for i in range(4):
+#            idc.split_sreg_range(funptr + i, "T", thumbReg, SR_user)
+        
+        idaapi.ida_funcs.add_func(funptr, idc.BADADDR)
+        rename_function(funptr, toCppName(name))
+        ida_bytes.create_strlit(nameptr, 0, ida_nalt.STRTYPE_TERMCHR)
+        
+        
+        #ida_bytes.create_data(offset, FF_DWORD, 4, ida_idaapi.BADADDR)
+        #ida_bytes.create_data(offset+4, FF_DWORD, 4, ida_idaapi.BADADDR)
+        ida_offset.op_plain_offset(offset, 1, 0)
+        ida_offset.op_plain_offset(offset+4, 1, 0)
+        
+        
+        
+        print(name)
+        print(funptr)
+
+        offset += 8
 def main():
     tbls = [
-            (0x00EAC450, 0x00EAC6DC)
+            0x810DDF04,
+            0x810DE104,
+            0x810DE12C,
+            0x810DE1D0,
+            0x810DE458,
+            0x810DE5A0,
+            0x810DE5B4,
+            0x810DE69C,
+            0x810DE6E8
            ]
     print("Running")
     
     for tbl in tbls:
-        rename_tbl(tbl[0], tbl[1])
+        rename_tbl(tbl)
 if __name__ == "__main__":
     main()
